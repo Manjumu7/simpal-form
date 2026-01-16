fd0d307042da8eb82c5a41fecefec653207c6195
Manjunath Madar <manjunathmu7@gmail.com>
2026-01-16 11:20:06 -0800

    added deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,88 @@
+
 import React, { useState } from 'react'
-import Card from './components/card'
+import Card from './components/Card'
 
-function App() {
-  const [userName, setuserName] = useState('')
-  const [UserRole, setUserRole] = useState('')
-  const [imageURL, setimageURL] = useState('')
-  const [userDesc, setuserDesc] = useState('')
+const App = () => {
 
-  const [AllUser, setAllUser] = useState([])
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [imageURL, setImageURL] = useState('')
+  const [userDesc, setUserDesc] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log(userName, imageURL, UserRole, userDesc)
 
-    const OldUser = [...AllUser]
-    OldUser.push([userName, UserRole, userDesc, imageURL])
-    console.log(OldUser)
+    setAllUsers([...allUsers, { userName, userRole, userDesc, imageURL }])
 
-    setAllUser(OldUser)
-
-    setuserName('')
+    setUserName('')
     setUserRole('')
-    setimageURL('')
-    setuserDesc('')
+    setUserDesc('')
+    setImageURL('')
   }
+
+  const deleteHandler = (idx) => {
+    const copyUsers = [...allUsers]
+
+    copyUsers.splice(idx, 1)
+
+    setAllUsers(copyUsers)
+  }
+
 
   return (
     <div className='h-screen bg-black text-white'>
-      <form
-        onSubmit={submitHandler}
-        className='p-2 flex flex-wrap '
-      >
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='px-2 py-2 flex flex-wrap'>
 
         <input
           value={userName}
-          onChange={(e) => setuserName(e.target.value)}
-          className='border-2 px-5 py-2 rounded m-2 w-[48%]'
+          onChange={(e) => {
+            setUserName(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder='enter your name'
-        />
+          placeholder='Enter your name' />
 
         <input
           value={imageURL}
-          onChange={(e) => setimageURL(e.target.value)}
-          className='border-2 px-5 py-2 rounded m-2 w-[48%]'
+          onChange={(e) => {
+            setImageURL(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder='image url'
-        />
+          placeholder='Image URL' />
 
         <input
-          value={UserRole}
-          onChange={(e) => setUserRole(e.target.value)}
-          className='border-2 px-5 py-2 rounded m-2 w-[48%]'
+          value={userRole}
+          onChange={(e) => {
+            setUserRole(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder='enter role'
-        />
+          placeholder='Enter Role' />
 
         <input
           value={userDesc}
-          onChange={(e) => setuserDesc(e.target.value)}
-          className='border-2 px-5 py-2 rounded m-2 w-[48%]'
+          onChange={(e) => {
+            setUserDesc(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder='enter description'
-        />
+          placeholder='Enter Description' />
 
-        <button className='px-5 py-2 text-xl font-semibold active:scale-95 cursor-pointer rounded bg-emerald-700 m-2 w-[92%]'>
-          create User
-        </button>
+
+        <button className=' px-5 py-2 text-xl active:scale-95 cursor-pointer font-semibold bg-emerald-700 rounded m-2 w-[97%]'>Create User</button>
       </form>
-      <div className='px-4 py-5 gap-4 flex flex-wrap'>
-        {AllUser.map(function (elem, idx) {
-          return (
-            <div
-              key={idx}
-              className='w-[20vw] rounded-xl py-6 px-6 flex flex-col items-center text-center h-80 bg-white text-black'
-            >
-              <img
-                className='h-24 w-24 rounded-full object-center object-cover'
-                src={elem[3]}
-                alt=""
-              />
+      <div className='px-4 py-10 gap-4 flex flex-wrap'>
 
-              <h1 className='text-2xl mt-2 font-semibold'>
-                {elem[0]}
-              </h1>
-
-              <h5 className='text-blue-500 text-semibold text-lg my-2'>
-                {elem[1]}
-              </h5>
-
-              <p className='font-medium text-sm leading-tight'>
-                {elem[2]}
-              </p>
-
-              <button className='px-4 py-2 rounded bg-red-600 text-xs active:scale-95 cursor-pointer'>
-                remove
-              </button>
-            </div>
-          )
+        {allUsers.map(function (elem, idx) {
+          return <Card idx={idx} elem={elem} deleteHandler={deleteHandler} />
         })}
-      </div>
 
+      </div>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    <div className='w-[20vw] rounded-xl py-6  px-6 flex flex-col items-center  text-center h-80 bg-white text-black'>
-        <img className='h-24 w-24 rounded-full' src="https://i.pinimg.com/736x/f5/a8/fe/f5a8fe04884fb2f26d31de731bf41c7e.jpg" alt="" />
-        <h1 className='text-2xl mt-2 font-semibold'>manjunath madar</h1>
-        <h5 className='text-blue-500 text-semibold text-lg my-2 '>developer</h5>
-        <p className='font-medium text-sm leading-tight'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, tempore?</p>
-        <button className='px-4 py-2 rounded bg-red-600 text-xs active:scale-95 cursor-pointer'>remove</button>
-    </div>
-  )
+const Card = (props) => {
+    return (
+        <div className='lg:w-[23vw] md:w-[30vw] sm:w-[45vw] rounded-xl py-8 px-8 flex items-center flex-col text-center bg-white text-black'>
+            <img className='h-24 w-24 rounded-full object-center object-cover' src={props.elem.imageURL} alt="" />
+            <h1 className='text-2xl mt-2 font-bold'>{props.elem.userName}</h1>
+            <h5 className=' text-blue-500 text-lg font-semibold my-2'>{props.elem.userRole}</h5>
+            <p className='text-sm font-medium leading-tight'>{props.elem.userDesc}</p>
+            <button onClick={() => {
+                props.deleteHandler(props.idx)
+            }} className='px-4 py-2 rounded text-xs cursor-pointer active:scale-95 bg-red-600 text-white font-semibold mt-3'>Remove</button>
+        </div>
+    )
 }
 
 export default Card
